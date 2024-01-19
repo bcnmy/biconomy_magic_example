52b27c014e241b5c840b147e9dbc227a10c27a4c
Venkatesh-Router <67518179+B-Venkatesh7210@users.noreply.github.com>
2024-01-19 22:29:10 +0530

    updated to latest Biconomy SDK

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { IPaymaster, BiconomyPaymaster } from "@biconomy/paymaster";
-import { IBundler, Bundler } from "@biconomy/bundler";
 import {
   BiconomySmartAccountV2,
-  DEFAULT_ENTRYPOINT_ADDRESS,
+  BiconomySmartAccountV2Config,
 } from "@biconomy/account";
 import {
   IHybridPaymaster,
@@ -11,42 +9,18 @@ import {
   PaymasterMode,
 } from "@biconomy/paymaster";
 import { ethers } from "ethers";
-import { ChainId } from "@biconomy/core-types";
-import {
-  ECDSAOwnershipValidationModule,
-  DEFAULT_ECDSA_OWNERSHIP_MODULE,
-} from "@biconomy/modules";
 import { Magic } from "magic-sdk";
 import { contractABI } from "../contract/contractABI";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 // Create a provider for the Polygon Mumbai network
 const provider = new ethers.providers.JsonRpcProvider(
   "https://rpc.ankr.com/polygon_mumbai"
 );
 
-
 // Specify the chain ID for Polygon Mumbai
 let chainId = 80001; // Polygon Mumbai or change as per your preferred chain
-
-// Create a Bundler instance
-const bundler: IBundler = new Bundler({
-  // get from biconomy dashboard https://dashboard.biconomy.io/
-  // for mainnet bundler url contact us on Telegram
-  bundlerUrl:
-    `https://bundler.biconomy.io/api/v2/${chainId}/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44`,
-  chainId: ChainId.POLYGON_MUMBAI, // or any supported chain of your choice
-  entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
-});
-
-
-// Create a Paymaster instance
-const paymaster: IPaymaster = new BiconomyPaymaster({
-  // get from biconomy dashboard https://dashboard.biconomy.io/
-  // Use this paymaster url for testing, you'll need to create your own paymaster for gasless transactions on your smart contracts.
-  paymasterUrl:
-    "https://paymaster.biconomy.io/api/v1/80001/-RObQRX9ei.fc6918eb-c582-4417-9d5a-0507b17cfe71",
-});
-
 
 export default function Home() {
   const [smartAccount, setSmartAccount] =
@@ -55,14 +29,15 @@ export default function Home() {
     null
   );
   const [count, setCount] = useState<string | null>(null);
+  const [txnHash, setTxnHash] = useState<string | null>(null);
 
   let magic: any;
 
   useEffect(() => {
     // Initialize the Magic instance
     //You can get your own API key by signing up for Magic here: https://dashboard.magic.link/signup
-    //Don't have an API KEY yet? Use this - "pk_live_DBDD65D080E15415"
-    magic = new Magic("pk_live_DBDD65D080E15415", {
+    //Don't have an API KEY yet? Use this - "pk_live_B3CC63B614156D0E"
+    magic = new Magic("pk_live_B3CC63B614156D0E", {
       network: {
         rpcUrl: "https://rpc.ankr.com/polygon_mumbai",
         chainId: 80001, // Polygon Mumbai or change as per your preferred chain
@@ -80,19 +55,18 @@ export default function Home() {
         "any"
       );
 
-      const ecdsaModule = await ECDSAOwnershipValidationModule.create({
+      const biconomySmartAccountConfig: BiconomySmartAccountV2Config = {
         signer: web3Provider.getSigner(),
-        moduleAddress: DEFAULT_ECDSA_OWNERSHIP_MODULE,
-      });
+        chainId: 80001,
+        biconomyPaymasterApiKey:
+          "-RObQRX9ei.fc6918eb-c582-4417-9d5a-0507b17cfe71",
+        bundlerUrl: `https://bundler.biconomy.io/api/v2/${chainId}/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44`,
+      };
 
-      let biconomySmartAccount = await BiconomySmartAccountV2.create({
-        chainId: ChainId.POLYGON_MUMBAI,
-        bundler: bundler,
-        paymaster: paymaster,
-        entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
-        defaultValidationModule: ecdsaModule,
-        activeValidationModule: ecdsaModule,
-      });
+      let biconomySmartAccount = await BiconomySmartAccountV2.create(
+        biconomySmartAccountConfig
+      );
+
       console.log(biconomySmartAccount);
       setSmartAccount(biconomySmartAccount);
       const address = await biconomySmartAccount.getAccountAddress();
@@ -115,46 +89,83 @@ export default function Home() {
   };
 
   const incrementCount = async () => {
-    const contractAddress = "0xc34E02663D5FFC7A1CeaC3081bF811431B096C8C";
-    const contractInstance = new ethers.Contract(
-      contractAddress,
-      contractABI,
-      provider
-    );
-    const minTx = await contractInstance.populateTransaction.increment();
-    console.log("Mint Tx Data", minTx.data);
-    const tx1 = {
-      to: contractAddress,
-      data: minTx.data,
-    };
-    let userOp = await smartAccount?.buildUserOp([tx1]);
-    console.log("UserOp", { userOp });
-    const biconomyPaymaster =
-      smartAccount?.paymaster as IHybridPaymaster<SponsorUserOperationDto>;
-    let paymasterServiceData: SponsorUserOperationDto = {
-      mode: PaymasterMode.SPONSORED,
-      smartAccountInfo: {
-        name: "BICONOMY",
-        version: "2.0.0",
-      },
-    };
-    const paymasterAndDataResponse =
-      await biconomyPaymaster?.getPaymasterAndData(
-        //@ts-ignore
-        userOp,
-        paymasterServiceData
+    try {
+      const toastId = toast("Populating Transaction", { autoClose: false });
+
+      const contractAddress = "0xc34E02663D5FFC7A1CeaC3081bF811431B096C8C";
+      const contractInstance = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        provider
       );
+      const minTx = await contractInstance.populateTransaction.increment();
+      console.log("Mint Tx Data", minTx.data);
+      const tx1 = {
+        to: contractAddress,
+        data: minTx.data,
+      };
 
-    //@ts-ignore
-    userOp.paymasterAndData = paymasterAndDataResponse.paymasterAndData;
-    //@ts-ignore
-    const userOpResponse = await smartAccount?.sendUserOp(userOp);
-    console.log("userOpHash", { userOpResponse });
-    //@ts-ignore
-    const { receipt } = await userOpResponse.wait(1);
-    console.log("txHash", receipt.transactionHash);
+      toast.update(toastId, { render: "Building UserOp", autoClose: false });
+      let userOp = await smartAccount?.buildUserOp([tx1]);
+      console.log("UserOp", { userOp });
+      const biconomyPaymaster =
+        smartAccount?.paymaster as IHybridPaymaster<SponsorUserOperationDto>;
+      let paymasterServiceData: SponsorUserOperationDto = {
+        mode: PaymasterMode.SPONSORED,
+        smartAccountInfo: {
+          name: "BICONOMY",
+          version: "2.0.0",
+        },
+      };
+      const paymasterAndDataResponse =
+        await biconomyPaymaster?.getPaymasterAndData(
+          //@ts-ignore
+          userOp,
+          paymasterServiceData
+        );
 
-    await getCountId();
+      //@ts-ignore
+      userOp.paymasterAndData = paymasterAndDataResponse.paymasterAndData;
+
+      //Add the below if statement if you encounter AA34 signature Error
+      if (
+        userOp &&
+        paymasterAndDataResponse.callGasLimit &&
+        paymasterAndDataResponse.verificationGasLimit &&
+        paymasterAndDataResponse.preVerificationGas
+      ) {
+        // Returned gas limits must be replaced in your op as you update paymasterAndData.
+        // Because these are the limits paymaster service signed on to generate paymasterAndData
+        // If you receive AA34 error check here..
+
+        userOp.callGasLimit = paymasterAndDataResponse.callGasLimit;
+        userOp.verificationGasLimit =
+          paymasterAndDataResponse.verificationGasLimit;
+        userOp.preVerificationGas = paymasterAndDataResponse.preVerificationGas;
+      }
+
+      toast.update(toastId, { render: "Sending UserOp", autoClose: false });
+      //@ts-ignore
+      const userOpResponse = await smartAccount?.sendUserOp(userOp);
+      console.log("userOpHash", { userOpResponse });
+      //@ts-ignore
+      const { receipt } = await userOpResponse.wait(1);
+      console.log("txHash", receipt.transactionHash);
+
+      if (receipt.transactionHash) {
+        toast.update(toastId, {
+          render: "Transaction Successful",
+          type: "success",
+          autoClose: 5000,
+        });
+        setTxnHash(receipt.transactionHash);
+      }
+
+      await getCountId();
+    } catch (error) {
+      console.log(error);
+      toast.error("Transaction Unsuccessful", { autoClose: 5000 });
+    }
   };
 
   return (
@@ -176,6 +187,7 @@ export default function Home() {
           {" "}
           <span>Smart Account Address</span>
           <span>{smartAccountAddress}</span>
+          <span>Network: Polygon Mumbai</span>
           <div className="flex flex-row justify-between items-start gap-8">
             <div className="flex flex-col justify-center items-center gap-4">
               <button
@@ -193,8 +205,19 @@ export default function Home() {
               >
                 Increment Count
               </button>
+              {txnHash && (
+                <a
+                  target="_blank"
+                  href={`https://mumbai.polygonscan.com/tx/${txnHash}`}
+                >
+                  <span className="text-white font-bold underline">
+                    Txn Hash
+                  </span>
+                </a>
+              )}
             </div>
           </div>
+          <span className="text-white">Open console to view console logs.</span>
         </>
       )}
     </main>
